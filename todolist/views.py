f1c45b57670eff84bd9b2c3e9bd53d1a7da10829
awohaomei <2590090372@qq.com>
2019-06-18 11:14:32 +0800

    修改命名空间

--- todolist/views.py
+++ todolist/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    return render(request, 'todolist/home.html')
 
 
 def edit(request):
-    return render(request,'edit.html')
+    return render(request, 'todolist/edit.html')
 
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'todolist/about.html')
