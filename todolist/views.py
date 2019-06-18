6c56b2cd09e37090c9536523b832ab94b1f01af2
awohaomei <2590090372@qq.com>
2019-06-18 15:51:27 +0800

    调整显示状态

--- todolist/views.py
+++ todolist/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
-def home(request):
-    return render(request, 'todolist/home.html')
+lst=[
+    {'待办事项': '吃饭','已完成':False},
+    {'待办事项': '睡觉','已完成':False}
+]
 
+def home(request):
+    global lst
+    if request.method=="POST":
+        content=request.POST.get('待办事项')
+        if not content or content.strip()=='':
+            return render(request, 'todolist/home.html',
+                          {'清单': lst,'警告':'请输入内容'})
+        else:
+            lst.append({'待办事项': content,'已完成':False})
+            return render(request,'todolist/home.html',{'清单':lst})
+    else:
+            return render(request, 'todolist/home.html', {'清单': lst})
 
 def edit(request):
     return render(request, 'todolist/edit.html')
@@ -11,3 +25,10 @@ def edit(request):
 
 def about(request):
     return render(request, 'todolist/about.html')
+
+def delete(request,forloop_counter):
+    global lst
+    forloop_counter=int(forloop_counter)-1
+    print(forloop_counter)
+    lst.pop(forloop_counter)
+    return redirect('todolist:主页')
